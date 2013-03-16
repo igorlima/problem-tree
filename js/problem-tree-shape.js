760f937b0fe9acfbcaa84b68e5771ee21c5dda4e
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-03-16 14:44:39 -0300

    Do refactoring and add text into shapes

---
+++ js/problem-tree-shape.js
@@ -0,0 +1,130 @@
+var ProblemTree = (function(ProblemTree) {
+
+  var Shape = ProblemTree.Shape = {},
+      r = ProblemTree.canvas(),
+      problem,
+      connections = [],
+      effects = [],
+      causes = []
+      color_problem = Raphael.getColor(),
+      color_cause = Raphael.getColor(),
+      color_effect = Raphael.getColor(),
+      DEFAULT = {
+        WIDTH: 30,
+        HEIGHT: 20
+      };
+
+  /********************************
+   * Public
+   ********************************/
+
+  Shape.problem = function() {
+    var color;
+
+    if (!problem) {
+      problem = r.rect(290, 80, DEFAULT.WIDTH*2, DEFAULT.HEIGHT*2, 10);
+      problem = config_shape(problem, color_problem);
+    }
+
+    return problem;
+
+  };
+
+  Shape.effect = function() {
+    var problem = Shape.problem(),
+        effect;
+
+    effect = r.ellipse(190, 100, DEFAULT.WIDTH, DEFAULT.HEIGHT);
+    effect = config_shape(effect, color_effect);
+    connect(effect, problem, "#000");
+
+    effects.push(effect);
+    return effect;
+
+  };
+
+  Shape.cause = function() {
+    var problem = Shape.problem(),
+        cause;
+
+    cause = r.ellipse(290, 180, DEFAULT.WIDTH, DEFAULT.HEIGHT);
+    cause = config_shape(cause, color_cause);
+    connect(cause, problem, "#000");
+
+    causes.push(cause);
+    return cause;
+
+  };
+
+
+
+  /********************************
+   * Private
+   ********************************/
+
+  function connect(obj1, obj2, color_line, color_background) {
+    var connection = r.connection(obj1, obj2, color_line, color_background);
+    connections.push(connection);
+
+    return connection;
+  }
+
+  function dragger() {
+    this.ox = this.type == "rect" ? this.attr("x") : this.attr("cx");
+    this.oy = this.type == "rect" ? this.attr("y") : this.attr("cy");
+    this.animate({"fill-opacity": .2}, 500);
+  }
+
+  function move(dx, dy) {
+    var att = this.type == "rect" ? {x: this.ox + dx, y: this.oy + dy} : {cx: this.ox + dx, cy: this.oy + dy};
+    this.attr(att);
+    move_text(this);
+    update_connections();
+    r.safari();
+  }
+
+  function up() {
+    this.animate({"fill-opacity": 0}, 500);
+  }
+
+  function update_connections() {
+    for (var i = connections.length; i--;) {
+      r.connection(connections[i]);
+    }
+  }
+
+  function config_shape(shape, color) {
+    shape.attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2, cursor: "move"});
+    shape.drag(move, dragger, up);
+    create_text(shape);
+    shape.dblclick(function() {
+      change_text(this.data('text'));
+    });
+    return shape;
+  }
+
+  function create_text(shape) {
+    var text = r.text(0, 0, "teste");
+    shape.data('text', text);
+    move_text(shape);
+    text.dblclick(function(){
+      change_text(this);
+    });
+  }
+
+  function move_text(shape) {
+    var text = shape.data("text"),
+        att = shape.type == "rect" ? {x: shape.attrs['x'] + shape.attrs['width']/2, y: shape.attrs['y'] + shape.attrs['height']/2} : {x: shape.attrs['cx'], y: shape.attrs['cy']};
+
+    text.attr(att);
+  }
+
+  function change_text(text) {
+    var new_text = prompt("Novo texto", text.attr('text'));
+    text.attr('text', new_text);
+  }
+
+  // Return the module
+  return ProblemTree;
+  
+}(ProblemTree));
--- js/problem-tree.js
+++ js/problem-tree.js
@@ -1,55 +1,22 @@
-;(function(window, Raphael) {
-  var r,
-      connections = [],
-      shapes;
-
-  var ProblemTreeClass = function() {
-    init();
-  };
-
-  var init = function() {
-    r = Raphael("holder", 640, 480);
-    shapes = [  r.ellipse(190, 100, 30, 20),
-                r.rect(290, 80, 60, 40, 10),
-                r.rect(290, 180, 60, 40, 2),
-                r.ellipse(450, 100, 20, 20)
-            ];
-
-    for (var i = 0, ii = shapes.length; i < ii; i++) {
-        var color = Raphael.getColor();
-        shapes[i].attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2, cursor: "move"});
-        shapes[i].drag(ProblemTreeClass.move, ProblemTreeClass.dragger, ProblemTreeClass.up);
-    }
-
-    connections.push(r.connection(shapes[0], shapes[1], "#000"));
-    connections.push(r.connection(shapes[1], shapes[2], "#000", "#fff|5"));
-    connections.push(r.connection(shapes[1], shapes[3], "#000", "#fff"));
-
-  };
-
-  ProblemTreeClass.fn = ProblemTreeClass.prototype;
+var ProblemTree = {
+  idElementHtml: 'holder',
+  width: 640,
+  height: 480
+};
 
 
-  ProblemTreeClass.dragger = function () {
-    this.ox = this.type == "rect" ? this.attr("x") : this.attr("cx");
-    this.oy = this.type == "rect" ? this.attr("y") : this.attr("cy");
-    this.animate({"fill-opacity": .2}, 500);
-  };
+ProblemTree = (function(ProblemTree, Raphael) {
+  var r;
 
-  ProblemTreeClass.move = function (dx, dy) {
-    var att = this.type == "rect" ? {x: this.ox + dx, y: this.oy + dy} : {cx: this.ox + dx, cy: this.oy + dy};
-    this.attr(att);
-    for (var i = connections.length; i--;) {
-      r.connection(connections[i]);
-    }
-    r.safari();
-  };
+  (function init() {
+    r = Raphael(ProblemTree['idElementHtml'], ProblemTree['width'], ProblemTree['height']);
+  })();
 
-  ProblemTreeClass.up = function () {
-    this.animate({"fill-opacity": 0}, 500);
+  ProblemTree.canvas = function() {
+    return r;
   };
 
 
-  window.problemTree = new ProblemTreeClass();  
-  
-})(window, Raphael);
+  return ProblemTree
+
+}(ProblemTree || {}, Raphael) );
