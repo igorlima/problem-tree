6b2f9cc811139aa14c60d601454909955f75b4ca
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-03-30 11:07:05 -0300

    Add a tree img as background

--- js/problem-tree.js
+++ js/problem-tree.js
@@ -1,13 +1,16 @@
 
 ;(function(window, Raphael) {
-  var r, fn,
+  var r, fn, img_problem_tree,
       DEFAULT = {
         WIDTH: 400,
-        HEIGHT: 380
+        HEIGHT: 380,
+        IMG_IDENT_X: 10,
+        IMG_IDENT_Y: 40
       };
 
   var ProblemTreeClass = window.ProblemTreeClass = function(attrs) {
-    var r = Raphael(attrs['idElementHtml'], attrs['width'] || DEFAULT.WIDTH, attrs['height'] || DEFAULT.HEIGHT);
+    var r = Raphael(attrs['idElementHtml'], attrs['width'] || DEFAULT.WIDTH, attrs['height'] || DEFAULT.HEIGHT),
+        img_problem_tree = r.image("img/img-canvas-problem-tree.png", DEFAULT.IMG_IDENT_X, DEFAULT.IMG_IDENT_Y, r.width - DEFAULT.IMG_IDENT_X, r.height - DEFAULT.IMG_IDENT_Y);
 
     this.attrs = attrs;
     this.canvas = function() {
